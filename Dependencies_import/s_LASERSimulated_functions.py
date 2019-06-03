3e5d740599d563d51d2b8843554c68e2588415cb
Clement Gouriou <clementgouriou@orange.com>
2019-06-04 00:06:17 +0200

    Improve the G2, G3 commands, implementing the other method XYIJ XYR QIJ. Only in the XY plane though.

--- Dependencies_import/s_LASERSimulated_functions.py
+++ Dependencies_import/s_LASERSimulated_functions.py
@@ -182,11 +182,75 @@ def doNEXTcmd(self):
     #print('NEXT: ',iter_varname, iter_varval)
     return None
 
-def G2Converter(self):
+def G2G3Converter(self):
+    '''
+    This method convert any type of G2 or G3 into the output of the 'PQR' method.
+    '''
     line, line_nbr = self.currentline
     codeline = self.dicGcodecmd[line_nbr]
     n = len(codeline)
-    # --- retrieve starting and ending angles, radius  --- #
+    # --- make method variable --- #
+    method = ''
+    # --- retrieve all possible parameters  --- #
+    for word in codeline:
+        if   word[0].upper() == 'P':
+            start_ang = self.evalWord(word[1:])
+            method   += 'P'
+        elif word[0].upper() == 'Q':
+            end_ang   = self.evalWord(word[1:])
+            method   += 'Q'
+        elif word[0].upper() == 'R':
+            R         = self.evalWord(word[1:])
+            method   += 'R'
+            R         = abs(R)
+        elif word[0].upper() == 'X':
+            end_point_x = self.evalWord(word[1:])
+            method   += 'X'
+        elif word[0].upper() == 'Y':
+            end_point_y = self.evalWord(word[1:])
+            method   += 'Y'
+        elif word[0].upper() == 'I':
+            center_point_i = self.evalWord(word[1:])
+            method   += 'I'
+        elif word[0].upper() == 'J':
+            center_point_j = self.evalWord(word[1:])
+            method   += 'J'
+    # --- action according to method --- #
+    current_pos = self.position.copy()
+    if   method == 'PQR':
+        R         = R
+        start_ang = start_ang
+        end_ang   = end_ang
+    elif method == 'XYIJ':
+        vect_init = np.array([current_pos[0]-center_point_i , current_pos[1]-center_point_j])
+        vect_fina = np.array([end_point_x   -center_point_i , end_point_y   -center_point_j])
+        R = np.sqrt( vect_init[0]**2 + vect_init[1]**2 )
+        start_ang = np.angle(vect_init[0] + 1j*vect_init[1]) * 180/np.pi
+        end_ang   = np.angle(vect_fina[0] + 1j*vect_fina[1]) * 180/np.pi
+    elif method == 'XYR':
+        vect_tot  = np.array([current_pos[0]-end_point_x , current_pos[1]-end_point_y])
+        vect_norm = np.sum(vect_tot**2)**0.5
+        vec_dir   = vect_tot/vect_tot_norm
+        vec_ortho = np.array([ vec_dir[1] , -vec_dir[0] ])
+        middle_pos= np.array([current_pos[0]+end_point_x , current_pos[1]+end_point_y]) * 0.5
+        h         = 0.5 * np.sqrt( 4*R**2 - vect_tot_norm**2 )
+        center_point = middle_pos + h*middle_pos
+        vect_init = current_pos - center_point
+        vect_fina = np.array([end_point_x, end_point_y]) - center_point
+        R         = R
+        start_ang = np.angle(vect_init[0] + 1j*vect_init[1]) * 180/np.pi
+        end_ang   = np.angle(vect_fina[0] + 1j*vect_fina[1]) * 180/np.pi
+    elif method == 'QIJ':
+        vect_init = np.array([current_pos[0]-center_point_i , current_pos[1]-center_point_j])
+        R = np.sqrt( vect_init[0]**2 + vect_init[1]**2 )
+        start_ang = np.angle(vect_init[0] + 1j*vect_init[1]) * 180/np.pi
+        end_ang   = end_ang
+    else:
+        print('Error: with method of G2, G3.\nOne might have forgotten or written something wrong.\nHere the method: {}'.format(method))
+        print('Codeline where is the error: ',codeline)
+        return None
+    # --- returning staring, ending angles and radius --- #
+    return R, start_ang, end_ang
 
 def doG2cmd(self):
     '''
@@ -201,14 +265,11 @@ def doG2cmd(self):
     codeline = self.dicGcodecmd[line_nbr]
     n = len(codeline)
     # --- retrieve starting and ending angles, radius  --- #
-    for word in codeline:
-        if   word[0].upper() == 'P':
-            start_ang = self.evalWord(word[1:])
-        elif word[0].upper() == 'Q':
-            end_ang   = self.evalWord(word[1:])
-        elif word[0].upper() == 'R':
-            R         = self.evalWord(word[1:])
-            R         = abs(R)
+    R_angl = self.G2G3Converter()
+    if R_angl == None:
+        return None
+    else:
+        R, start_ang, end_ang = R_angl
     # --- make arguments --- #
     theta_start = start_ang *np.pi/180. # the command G2 recieve deg unity # since it is counter-clockwise
     theta_end   = end_ang   *np.pi/180.
@@ -244,14 +305,11 @@ def doG3cmd(self):
     codeline = self.dicGcodecmd[line_nbr]
     n = len(codeline)
     # --- retrieve starting and ending angles, radius  --- #
-    for word in codeline:
-        if   word[0].upper() == 'P':
-            start_ang = self.evalWord(word[1:])
-        elif word[0].upper() == 'Q':
-            end_ang   = self.evalWord(word[1:])
-        elif word[0].upper() == 'R':
-            R         = self.evalWord(word[1:])
-            R         = abs(R)
+    R_angl = self.G2G3Converter()
+    if R_angl == None:
+        return None
+    else:
+        R, start_ang, end_ang = R_angl
     # --- make arguments --- #
     theta_start = start_ang *np.pi/180. # the command G2 recieve deg unity # since it is counter-clockwise
     theta_end   = end_ang   *np.pi/180.
@@ -301,3 +359,94 @@ def doG92cmd(self):
     self.setLaserPosition( self.position + self.origin - new_origin )
     self.setLaserOrigin( new_origin )
 
+################################
+
+def doG2cmd_old(self):
+    '''
+    G2: CW (clockwise) circular interpolation on coordinate system 1
+    clockwise = counter-trogonometric
+    Here we only implement the:
+        G2 P.. Q.. R..
+    where P in the begining angle, ang Q the final one in a circle of radius R that has starting point
+    the position we are in.
+    '''
+    line, line_nbr = self.currentline
+    codeline = self.dicGcodecmd[line_nbr]
+    n = len(codeline)
+    # --- retrieve starting and ending angles, radius  --- #
+    for word in codeline:
+        if   word[0].upper() == 'P':
+            start_ang = self.evalWord(word[1:])
+        elif word[0].upper() == 'Q':
+            end_ang   = self.evalWord(word[1:])
+        elif word[0].upper() == 'R':
+            R         = self.evalWord(word[1:])
+            R         = abs(R)
+    # --- make arguments --- #
+    theta_start = start_ang *np.pi/180. # the command G2 recieve deg unity # since it is counter-clockwise
+    theta_end   = end_ang   *np.pi/180.
+    Theta    = theta_start - theta_end
+    pos_init = self.position.copy() # + np.array([R*np.sin(Theta), R*(1-np.cos(Theta)), 0])
+    args     = [pos_init+self.origin, theta_start, theta_end, R, self.coordmode, self.isON] 
+    # --- add new instruction --- #
+    N = len( list(self.dicinstruction.keys()) ) # current number of instructions
+    self.dicinstruction[N+1] = ['G2', args] # we add instruction after instruction, because we made the lecture of the gcode linear in the executio, ie not in parallel.
+    # --- update current position --- #
+    displacement  = np.array([R*np.cos(theta_end), R*np.sin(theta_end), 0]) - np.array([R*np.cos(theta_start), R*np.sin(theta_start), 0])
+    self.position = self.position.copy() + displacement
+    # --- add coord to waveguide --- #
+    if self.isON:
+        dx = 0.1
+        L  = 2*R*Theta
+        N  = int(np.max([L/dx, 10]))
+        if   theta_start < theta_end: # careful we are clockwise !
+            theta_var = np.linspace(theta_start+2*np.pi, theta_end, N)
+        elif theta_start >= theta_end:
+            theta_var = np.linspace(theta_start, theta_end, N)
+        x  = R*np.cos(theta_var)
+        y  = R*np.sin(theta_var)
+        for i in range(N):
+            self.current_WG.append( pos_init+self.origin + np.array([x[i], y[i], 0.]) - np.array([R*np.cos(theta_start), R*np.sin(theta_start), 0]))
+
+def doG3cmd_old(self):
+    '''
+    G3: CCW (counter-clockwise) circular interpolation on coordinate system 1
+    counter-clockwise = trogonometric
+    '''
+    line, line_nbr = self.currentline
+    codeline = self.dicGcodecmd[line_nbr]
+    n = len(codeline)
+    # --- retrieve starting and ending angles, radius  --- #
+    for word in codeline:
+        if   word[0].upper() == 'P':
+            start_ang = self.evalWord(word[1:])
+        elif word[0].upper() == 'Q':
+            end_ang   = self.evalWord(word[1:])
+        elif word[0].upper() == 'R':
+            R         = self.evalWord(word[1:])
+            R         = abs(R)
+    # --- make arguments --- #
+    theta_start = start_ang *np.pi/180. # the command G2 recieve deg unity # since it is counter-clockwise
+    theta_end   = end_ang   *np.pi/180.
+    Theta    = theta_end - theta_start
+    pos_init = self.position.copy()
+    args     = [pos_init+self.origin, theta_start, theta_end, R, self.coordmode, self.isON] 
+    # --- add new instruction --- #
+    N = len( list(self.dicinstruction.keys()) ) # current number of instructions
+    self.dicinstruction[N+1] = ['G3', args] # we add instruction after instruction, because we made the lecture of the gcode linear in the executio, ie not in parallel.
+    # --- update current position --- #
+    displacement  = np.array([R*np.cos(theta_end), R*np.sin(theta_end), 0]) - np.array([R*np.cos(theta_start), R*np.sin(theta_start), 0])
+    self.position = self.position.copy() + displacement
+    # --- add coord to waveguide --- #
+    if self.isON:
+        dx = 0.1
+        L  = 2*R*Theta
+        N  = int(np.max([L/dx, 10]))
+        if   theta_start <= theta_end: # careful we are counter-clockwise !
+            theta_var = np.linspace(theta_start, theta_end, N)
+        elif theta_start >  theta_end:
+            theta_var = np.linspace(theta_start, theta_end+2*np.pi, N)
+        x  = R*np.cos(theta_var)
+        y  = R*np.sin(theta_var)
+        for i in range(N):
+            self.current_WG.append( pos_init+self.origin + np.array([x[i], y[i], 0.]) - np.array([R*np.cos(theta_start), R*np.sin(theta_start), 0]))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 GUI application to generate GCode and simulate it.
+
+github package at: https://github.com/Clemgou/PyQt5_GUI_FLDW
